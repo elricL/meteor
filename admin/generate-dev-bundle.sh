7e60a25486a0771196c2e7c370757b0745ed92f6
matt debergalis <deberg@gmail.com>
2012-08-13 14:32:06 -0700

    include simplesmtp and mailcomposer NPM packages.

--- admin/generate-dev-bundle.sh
+++ admin/generate-dev-bundle.sh
@@ -143,6 +143,8 @@ npm install fibers@0.6.5
 npm install useragent@1.0.6
 npm install request@2.9.202
 npm install http-proxy@0.8.0
+npm install simplesmtp@0.1.19
+npm install mailcomposer@0.1.15
 
 # unused, but kept in bundle for compatibility for a while.
 npm install connect-gzip@0.1.5
