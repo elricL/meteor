050b96610eb3aaa029394de68e20d607506f4855
David Glasser <glasser@davidglasser.net>
2012-08-14 12:40:19 -0700

    Basic SMTP mail-sending support.

--- packages/email/email.js
+++ packages/email/email.js
@@ -4,16 +4,70 @@ Email = {};
   var Future = __meteor_bootstrap__.require('fibers/future');
   // js2-mode AST blows up when parsing 'future.return()', so alias.
   Future.prototype.ret = Future.prototype.return;
-
+  var urlModule = __meteor_bootstrap__.require('url');
   var MailComposer = __meteor_bootstrap__.require('mailcomposer').MailComposer;
 
-  if (process.env.MAIL_URL)
-    throw new Error("SMTP not yet implemented");
+  var makePool = function (mailUrlString) {
+    var mailUrl = urlModule.parse(mailUrlString);
+    if (mailUrl.protocol !== 'smtp:')
+      throw new Error("Email protocol in $MAIL_URL (" +
+                      mailUrlString + ") must be 'smtp'");
+
+    var port = +(mailUrl.port);
+    var auth = false;
+    if (mailUrl.auth) {
+      var parts = mailUrl.auth.split(':', 2);
+      auth = {user: parts[0] && decodeURIComponent(parts[0]),
+              pass: parts[1] && decodeURIComponent(parts[1])};
+    }
+
+    var simplesmtp = __meteor_bootstrap__.require('simplesmtp');
+    return simplesmtp.createClientPool(
+      port,  // Defaults to 25
+      mailUrl.hostname,  // Defaults to "localhost"
+      { secureConnection: (port === 465),
+        // XXX allow maxConnections to be configured?
+        auth: auth });
+  };
+
+  var smtpPool = null;
+  if (process.env.MAIL_URL) {
+    smtpPool = makePool(process.env.MAIL_URL);
+  };
 
   Email._next_devmode_mail_id = 0;
 
   // Overridden by tests.
   Email._output_stream = process.stdout;
+
+  var devModeSend = function (mc) {
+    var devmode_mail_id = Email._next_devmode_mail_id++;
+
+    // This approach does not prevent other writers to stdout from interleaving.
+    Email._output_stream.write("====== BEGIN MAIL #" + devmode_mail_id +
+                               " ======\n");
+    mc.streamMessage();
+    mc.pipe(Email._output_stream, {end: false});
+    var future = new Future;
+    mc.on('end', function () {
+      Email._output_stream.write("====== END MAIL #" + devmode_mail_id +
+                                 " ======\n");
+      future.ret();
+    });
+    future.wait();
+  };
+
+  var smtpSend = function (mc) {
+    var future = new Future;
+    smtpPool.sendMail(mc, function (err, responseObj) {
+      future.ret([err, responseObj]);
+    });
+    var errAndResponse = future.wait();
+    // XXX figure out error handling
+    if (errAndResponse[0])
+      throw errAndResponse[0];
+    console.log(errAndResponse[1]);
+  };
 
   /**
    * Send an email.
@@ -47,21 +101,11 @@ Email = {};
       text: options.text
     });
 
-    var devmode_mail_id = Email._next_devmode_mail_id++;
-
-    // This approach does not prevent other writers to stdout from interleaving.
-    Email._output_stream.write("====== BEGIN MAIL #" + devmode_mail_id +
-                               " ======\n");
-    mc.streamMessage();
-    // XXX support SMTP
-    mc.pipe(Email._output_stream, {end: false});
-    var future = new Future;
-    mc.on('end', function () {
-      Email._output_stream.write("====== END MAIL #" + devmode_mail_id +
-                                 " ======\n");
-      future.ret();
-    });
-    future.wait();
+    if (smtpPool) {
+      smtpSend(mc);
+    } else {
+      devModeSend(mc);
+    }
   };
 
 })();
